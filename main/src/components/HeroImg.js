fe941f6888be623d5efb81aba19959c052a3b779
jlmayo <jlmayo22@gmail.com>
2022-12-31 02:52:00 -0700

    HeroImgs

--- main/src/components/HeroImg.js
+++ main/src/components/HeroImg.js
@@ -1,9 +1,28 @@
 import "./HeroImageStyles.css";
-
 import React from "react";
+import { Link } from "react-router-dom";
+import IntroImg from "../assets/CoffeeAndCodingLg.jpg";
 
 const HeroImage = () => {
-  return <div>HeroImage</div>;
+  return (
+    <div className="hero">
+      <div className="mask">
+        <img className="intro-img" src={IntroImg} alt="IntroImg" />
+      </div>
+      <div className="content">
+        <p>Jennifer Mayo</p>
+        <h1>Student Developer</h1>
+        <div>
+          <Link to="/project" className="btn">
+            Projects
+          </Link>
+          <Link to="/contact" className="btn btn-light">
+            Contact
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroImage;
