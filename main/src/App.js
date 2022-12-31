c0535d438b1a8051aca56fda4d1ee954f485e0d4
jlmayo <jlmayo22@gmail.com>
2022-12-30 21:26:13 -0700

    Navbar styling. File reorg.

--- main/src/App.js
+++ main/src/App.js
@@ -4,7 +4,6 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Project from './pages/Project'
 import Contact from './pages/Contact'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 import { Routes, Route } from 'react-router-dom'
 
--- main/src/components/Navbar.js
+++ main/src/components/Navbar.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavbarStyles.css";
 import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  
+  
   return (
     <div className="header">
       <Link to="/">
@@ -23,6 +29,15 @@ const Navbar = () => {
           <Link to="/contact">Contact</Link>
         </li>
       </ul>
+      <div className="hamburger" onClick={handleClick}>
+        {click ? (
+        <FaTimes size={20} style={{ color: "#fff" }} />
+        ) :
+        (
+        <FaBars size={20} style={{ color: "#fff" }} />
+        )}
+        
+      </div>
     </div>
   );
 };
