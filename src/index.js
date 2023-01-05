cd8da47cad501a43bd9a7556c51c3d7ad7bb5fec
jlmayo <jlmayo22@gmail.com>
2023-01-05 09:14:10 -0700

    basename React-Portfolio. It works!

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter basename='/index.html'>
+  <BrowserRouter basename='/React-Portfolio'>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
